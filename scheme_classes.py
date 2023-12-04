b91e7934effeb7c79269f2f2603cd7ffb20d124e
Jaanavi Thanamala <51378173+jaanavit@users.noreply.github.com>
2023-12-03 16:14:48 -0800

    Update scheme_classes.py

--- scheme_classes.py
+++ scheme_classes.py
@@ -55,7 +55,12 @@ class Frame:
         if len(formals) != len(vals):
             raise SchemeError('Incorrect number of arguments to function call')
         # BEGIN PROBLEM 8
-        "*** YOUR CODE HERE ***"
+        new_frame = Frame(self)
+        while formals != nil:
+            new_frame.define(formals.first, vals.first)
+            formals = formals.rest
+            vals = vals.rest
+        return new_frame
         # END PROBLEM 8
 
 ##############
