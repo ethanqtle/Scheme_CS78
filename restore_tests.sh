486c917f82ce374759ba52e61c243ecb41ad7cee
Ethan Le <ethanqtle@gmail.com>
2023-12-05 14:59:16 -0800

    Add script to restore tests/*.py for team member to learn by answering the questions from the tests/*.py

---
+++ restore_tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+function my_cmd()
+{
+  test_case=$1
+  echo "Restoring test case tests/${test_case}.py"
+  cp tests.unlocked/${test_case}.py tests/${test_case}.py
+}
+for test_case in {01..16}; do
+  my_cmd $test_case
+done
+
+my_cmd eval_apply
