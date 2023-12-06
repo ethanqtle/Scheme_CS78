28a4c041bb3a531ffc640d67dc3c5fadd723ccb3
Ethan Le <ethanqtle@gmail.com>
2023-12-05 16:35:18 -0800

    Add comments for bash script

--- restore_tests.sh
+++ restore_tests.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 function my_cmd()
 {
+  # $1 is the test case number
+  # the actual test files are in tests.locked/${test_case}.py
+  # and the test files that are run are in tests/${test_case}.py
+  # so test_case is the base name of the test file
   test_case=$1
   echo "Restoring test case tests/${test_case}.py"
   cp tests.locked/${test_case}.py tests/${test_case}.py
 }
+
+# iterate over all test cases from 01 to 16
 for test_case in {01..16}; do
+  # run my_cmd function on each test case number
   my_cmd $test_case
 done
 
+# restore eval_apply test case
 my_cmd eval_apply
--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,7 +1,14 @@
-#!env bash
+#!/usr/bin/env bash
 
+# iterate over all test cases from 01 to 16
 for test_case in {01..16}; do
+  # run ok on each test case number
+  # --local means run ok locally w/o submitting to okpy.org
+  # -q is option to pick the test case (01, 02, etc. or eval_apply)
   python ok --local -q $test_case
 done
 python ok --local -q eval_apply
+
+# run ok on the scheme tests.scm
+# a scheme test is a test that is written in scheme
 python ok --local -q tests.scm
